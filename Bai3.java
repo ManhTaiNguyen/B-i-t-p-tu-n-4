e8e839f0b27c74af9a3cd2068c6ee2d5a12be467
Nguyễn Mạnh Tài <124764533+ManhTaiNguyen@users.noreply.github.com>
2023-03-06 20:21:25 +0700

    Update Bai3.java

--- Bai3.java
+++ Bai3.java
@@ -2,22 +2,20 @@ import java.util.Scanner;
  
 public class Bai3{
     public static void main(String[] args){
-        int age, year;
-        String name;
         Scanner sc = new Scanner(System.in);
         System.out.println("Nhập tên: ");
-        name = sc.nextLine();
+        String name = sc.nextLine();
         System.out.println("Nhập năm sinh: ");
-        year = sc.nextInt();
-        age = 2023 - year;
+        int year = sc.nextInt();
+        int age = 2023 - year;
         if (age < 16){
-            System.out.println("Bạn " + name + "ở độ tuổi vị thành niên");
+            System.out.println("Bạn " + name + " ở độ tuổi vị thành niên");
         }
         else if (age >= 16 && age < 18){
-            System.out.println("Bạn " + name + "ở độ tuổi trưởng thành");
+            System.out.println("Bạn " + name + " ở độ tuổi trưởng thành");
         } 
         else{
-            System.out.println("Bạn " + name +  "đã già");
+            System.out.println("Bạn " + name +  " đã già");
         }
     }
 }
