0ea2aac17aac973f31df8ecafeb6aebaeaf5fd05
Nguyễn Mạnh Tài <124764533+ManhTaiNguyen@users.noreply.github.com>
2023-03-06 20:18:22 +0700

    Update Bai2.java

--- Bai2.java
+++ Bai2.java
@@ -1,14 +1,13 @@
 import java.util.Scanner;
 
-public class Bai2 {
-    public static void main(String[] args) {
-        int n;
+public class Bai2{
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Nhập n: ");
-        n = sc.nextInt();
+        System.out.println("Nhập số bất kì: ");
+        int n = sc.nextInt();
         if (n % 2 == 0) 
-            System.out.println(n + " là số chẵn");
+            System.out.println("Số " + n + " là số chẵn");
         else 
-            System.out.println(n + " là số lẻ");
+            System.out.println("Số " + n + " là số lẻ");
     }  
 }
